f30c73bcac5cc18c637498057b190e5497dab700
Angelica Bicalho <angelicanarcisob@hotmail.com>
2026-05-11 13:27:47 -0300

    commit

--- script.js
+++ script.js
@@ -1,19 +1,102 @@
+const video = document.getElementById("videoIntro");
+const intro = document.getElementById("intro");
+const convite = document.getElementById("convite");
+const btnPlay = document.getElementById("btnPlay");
+
+convite.style.display = "none";
+
+/* VIDEO */
+
+btnPlay.addEventListener("click", () => {
+
+  video.play();
+
+  btnPlay.style.display = "none";
+
+});
+
+video.addEventListener("ended", () => {
+
+  intro.style.display = "none";
+
+  convite.style.display = "block";
+
+});
+
+/* CAMPOS DINÂMICOS */
+
+const quantidadeInput = document.getElementById("quantidade");
+const camposDinamicos = document.getElementById("camposDinamicos");
+
+quantidadeInput.addEventListener("input", () => {
+
+  camposDinamicos.innerHTML = "";
+
+  let quantidade = parseInt(quantidadeInput.value);
+
+  if(isNaN(quantidade) || quantidade <= 0){
+    return;
+  }
+
+  for(let i = 1; i <= quantidade; i++){
+
+    camposDinamicos.innerHTML += `
+    
+      <div class="convidado-box">
+
+        <h3>Convidado ${i}</h3>
+
+        <input 
+          type="text" 
+          placeholder="Nome do convidado"
+          class="nomeConvidado"
+        >
+
+        <input 
+          type="number" 
+          placeholder="Idade"
+          class="idadeConvidado"
+        >
+
+      </div>
+
+    `;
+
+  }
+
+});
+
+/* BOTÃO */
+
 const botao = document.getElementById("btnConfirmar");
 
 botao.addEventListener("click", confirmar);
 
 function confirmar() {
 
-  let nome = document.getElementById("nome").value;
-  let pessoas = document.getElementById("pessoas").value;
+  let quantidade = quantidadeInput.value;
 
-  if (nome === "" || pessoas === "") {
-    alert("Preencha todos os campos!");
+  if(quantidade === ""){
+    alert("Informe a quantidade!");
     return;
+  }
+
+  let nomes = document.querySelectorAll(".nomeConvidado");
+  let idades = document.querySelectorAll(".idadeConvidado");
+
+  for(let i = 0; i < nomes.length; i++){
+
+    if(nomes[i].value === "" || idades[i].value === ""){
+
+      alert("Preencha todos os convidados!");
+      return;
+
+    }
+
   }
 
   document.getElementById("mensagem").style.display = "block";
 
-  console.log("Convidado:", nome);
-  console.log("Quantidade:", pessoas);
+  alert("Presença confirmada com sucesso! 🎉");
+
 }
